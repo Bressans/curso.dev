16c7b4efdcce1f58f8b4d25f8978ee0dd9100821
Bressans <joao.pedro.bressan@hotmail.com>
2024-12-10 03:49:33 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   console.log("Credenciais do postgres: ", {
     host: process.env.POSTGRES_HOST,
@@ -19,8 +20,8 @@ async function query(queryObject) {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
+    console.error(error);
     throw error;
   } finally {
     await client.end();
